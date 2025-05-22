b56d08b880cfd20a48b242584873c61bb8d0cec5
Nicolas Torre <10921378+nicolastorre@users.noreply.github.com>
2025-05-22 09:16:19 +0200

    feat: add src folder and clean App entry

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={styles.backgroundStyle.backgroundColor}
+      />
+      <Text>Habit Tracker</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: 'white',
+  },
+});
+
+export default App;
